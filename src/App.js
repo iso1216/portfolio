4892ed3cba4f30ea8fb84492497d6b8f381df1ad
Taishi <taishi2002i@gmail.com>
2023-09-14 14:29:01 +0900

    調整

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
-import { Container } from '@mui/material';
 
 function App() {
   const [value, setValue] = React.useState(0);
   return (
-    <Container>
+    <>
       <Header setValue={setValue} />
       <Main value={value} />
       <Footer />
-    </Container>
+    </>
   );
 }
 
