42b3fe4650c504f7f245842329e50040d5c33c66
Taishi <taishi2002i@gmail.com>
2023-09-20 11:16:47 +0900

    エラー除去

--- src/components/History.js
+++ src/components/History.js
@@ -6,14 +6,15 @@ export default function History() {
   return (
     <Container maxWidth='lg'>
       <h1>経歴</h1>
-      <Box sx={{display: 'flex', justifyContent: 'center'}}>
-        <Box sx={{width: '80%'}}>
-          {HistData.map((history) => (
-            <Box sx={{"@media screen and (min-width:700px)": {my: 3}}}>
-            <Box sx={{"@media screen and (min-width:700px)": {display: 'flex'}}}>
-            <Typography sx={{"@media screen and (min-width:400px)": { fontSize: 20 }}} marginRight={2}>{history.Date}</Typography>
-            <Typography fontSize={{"xs": '', "sm": 20}}>{history.name}</Typography></Box>
-            {history.detail ? <Typography fontSize={{"xs": 10, "sm": 15}} marginLeft={5}>*{history.detail}</Typography> : ''}
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Box>
+          {HistData.map((history, index) => (
+            <Box key={index} sx={{"@media screen and (min-width:700px)": {my: 3}}}>
+              <Box sx={{"@media screen and (min-width:700px)": {display: 'flex'}}}>
+                <Typography sx={{"@media screen and (min-width:400px)": { fontSize: 20 }}} marginRight={2}>{history.Date}</Typography>
+                <Typography fontSize={{"xs": '', "sm": 20}}>{history.name}</Typography>
+              </Box>
+              {history.detail ? <Typography fontSize={{"xs": 10, "sm": 15}} marginLeft={5}>*{history.detail}</Typography> : ''}
             </Box>
           ))}
         </Box>
