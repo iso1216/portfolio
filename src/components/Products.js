39590674b8f0ca77bb6e559f58d96c0636026501
Taishi <taishi2002i@gmail.com>
2023-09-21 11:14:15 +0900

    情報の追加

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,8 @@
-import { Box, Container, Link, Typography } from "@mui/material";
+import { Box, Container, Link, Typography, useMediaQuery } from "@mui/material";
 import { productsData } from "../Data/productsData";
 
 export default function Products(){
+  const matches = useMediaQuery('(min-width:400px)');
   return(
     <Container maxWidth='lg'>
       <h1>製作物</h1>
@@ -9,15 +10,27 @@ export default function Products(){
         <Box width={'70%'}>
         {productsData.map((product, index) => (
           <Box key={index} marginBottom={3}>
-            <Typography variant="h4" paddingBottom={1}>{product.name}</Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Box sx={{width: '80%'}}>
-            <Typography variant="body1" paddingBottom={1}>{product.detail}</Typography>
-            github：<Link href={product.url} target="_blank" underline="none">{product.url}</Link><br/>
-            {product.url2 ? <>アプリ：<Link href={product.url2} target="_blank" underline="none">{product.url2}</Link><br/></> : ''}
-            {product.img ? <img src={product.img} alt={product.name} className="productImage"/> : ''}
-            </Box>
-            </Box>
+          { matches ? 
+            <><Typography variant="h4" paddingBottom={1}>{product.name}</Typography><Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Box sx={{ width: '80%' }}>
+                  <Typography variant="h6" paddingBottom={1}>{product.explanation}</Typography>
+                  <Typography variant="body1" sx={{ whiteSpace: 'pre-line' }}>{product.detail}</Typography>
+                  github：<Link href={product.url} target="_blank" underline="none" sx={{ "@media screen and (max-width:400px)": { fontSize: 12 } }}>{product.url}</Link><br />
+                  {product.url2 ? <>アプリ：<Link href={product.url2} target="_blank" underline="none" sx={{ "@media screen and (max-width:400px)": { fontSize: 12 } }}>{product.url2}</Link><br /></> : ''}
+                  {product.img ? <img src={product.img} alt={product.name} className="productImage" /> : ''}
+                </Box>
+              </Box></>
+            : <><Typography variant="h6" paddingBottom={1}>{product.name}</Typography><Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Box sx={{ width: '80%' }}>
+                  <Typography variant="body1" paddingBottom={1}>{product.explanation}</Typography>
+                  <Typography variant="body2" sx={{ whiteSpace: 'pre-line' }}>{product.detail}</Typography>
+                  github：<Link href={product.url} target="_blank" underline="none" sx={{ "@media screen and (max-width:400px)": { fontSize: 12 } }}>{product.url}</Link><br />
+                  {product.url2 ? <>アプリ：<Link href={product.url2} target="_blank" underline="none" sx={{ "@media screen and (max-width:400px)": { fontSize: 12 } }}>{product.url2}</Link><br /></> : ''}
+                  {product.img ? <img src={product.img} alt={product.name} className="productImage" /> : ''}
+                </Box>
+              </Box></>
+
+          }
           </Box>
         ))}
         </Box>
