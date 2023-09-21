8b3261bda5977575a7ade0afffab5f4c44f8b7b7
Taishi <taishi2002i@gmail.com>
2023-09-21 12:41:31 +0900

    雑ホーム

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,20 @@
-import { Box } from '@mui/material';
+import React from 'react';
+import { Container, Typography, useMediaQuery } from '@mui/material';
 
-export default function Home(){
-  return(
-    <Box>
-    </Box>
+export default function Home() {
+  const matches = useMediaQuery('(min-width:700px)');
+
+  return (
+    <Container maxWidth='md' sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
+      {matches ? (
+        <Typography variant="h2" sx={{ marginBottom: 2, textAlign: 'center' }}>
+          Welcome to My Portfolio
+        </Typography>
+      ) : (
+        <Typography variant="h4" sx={{ marginBottom: 2, textAlign: 'center' }}>
+          Welcome to My Portfolio
+        </Typography>
+      )}
+    </Container>
   );
 }
