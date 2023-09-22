1925553aa703367e9e9e53f433c6c8abe292907d
Taishi <taishi2002i@gmail.com>
2023-09-22 15:06:52 +0900

    画像

--- src/Data/itemData.js
+++ src/Data/itemData.js
@@ -1,26 +1,26 @@
 export const itemData = [
   {
-    img: '%PUBLIC_URL%/img/pokemon.jpg',
+    img: '/portfolio/img/pokemon.jpg',
     title: 'pokemonSleep',
   },
   {
-    img: '%PUBLIC_URL%/img/sutaba.jpg',
+    img: '/portfolio/img/sutaba.jpg',
     title: 'sutaba',
   },
   {
-    img: '%PUBLIC_URL%/img/noodle.jpg',
+    img: '/portfolio/img/noodle.jpg',
     title: 'kanayamaya',
   },
   {
-    img: '%PUBLIC_URL%/img/karaoke.jpg',
+    img: '/portfolio/img/karaoke.jpg',
     title: 'karaoke',
   },
   {
-    img: '%PUBLIC_URL%/img/huro.jpg',
+    img: '/portfolio/img/huro.jpg',
     title: 'huro',
   },
   {
-    img: '%PUBLIC_URL%/img/riza.jpg',
+    img: '/portfolio/img/riza.jpg',
     title: 'riza-don',
   },
 ];
