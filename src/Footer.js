b94985202cd817aeadd73b0bb4b918061b6f4aae
Taishi <taishi2002i@gmail.com>
2023-09-22 12:38:42 +0900

    フッター完成

--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,5 @@
-import { AppBar, Box, Container, Grid, IconButton, Link, Typography } from "@mui/material";
+import { AppBar, Box, IconButton, Link, Typography } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
@@ -14,35 +15,48 @@ export default function Footer({setValue}) {
 
   return (
     <AppBar component="footer" position="static" color="inherit">
-    <Container maxWidth='md'>
-      <Grid container sx={{padding: 1}}>
-        <Grid md={5} lg={4} sx={{display: {"xs": "none", "md": "flex"}}}>
-          <Typography variant="caption" component={'a'} href={'/'} padding={1} color={"inherit"} sx={{textDecoration: 'none'}}>© 2023 Taishi Isomura</Typography>
+    <Box>
+      <Grid container marginY={2}>
+        <Grid xs={12} md={4} textAlign='center'>
+          <Typography component='a' href={'/'} color={"inherit"} sx={{textDecoration: 'none'}}>© 2023 Taishi Isomura</Typography>
         </Grid>
-        <Grid container xs={12} md={7} lg={8}>
-          <Grid xs={2} sm={3}>
-            <Box component="ul" aria-labelledby="category-a" fontSize={14}>
-              <li><Link underline="none" color="inherit" onClick={() => {handleClick(1)}}>Profile</Link></li>
-              <li><Link underline="none" color="inherit" onClick={() => {handleClick(2)}}>Products</Link></li>
-              <li><Link underline="none" color="inherit" onClick={() => {handleClick(3)}}>History</Link></li>
+        <Grid container xs={12} md={8}>
+          <Grid xs={3}>
+            <Box sx={{display: 'flex', justifyContent: 'center', "@media screen and (max-width:490px)": {fontSize: 12}}}>
+              <Box component="ul" aria-labelledby="category-a" sx={{ pl: 2 }}>
+                <li><Link underline="none" color="inherit" onClick={() => {handleClick(1)}}>Profile</Link></li>
+                <li><Link underline="none" color="inherit" onClick={() => {handleClick(2)}}>Products</Link></li>
+                <li><Link underline="none" color="inherit" onClick={() => {handleClick(3)}}>History</Link></li>
+              </Box>
             </Box>
           </Grid>
-          <Grid xs={6} sm={5}>
-            <Box component="ul" aria-labelledby="category-b" fontSize={14}>
-              {productsData.map((product, index) =>(
-                <li><Link underline="none" color="inherit" href={product.url} target="_blank" key={index}>{product.name}</Link></li>
-              ))}
+          <Grid xs={5}>
+            <Box sx={{display: 'flex', justifyContent: 'center', "@media screen and (max-width:490px)": {fontSize: 12}}}>
+              <Box component="ul" aria-labelledby="category-b" sx={{ pl: 2 }}>
+                {productsData.map((product, index) =>(
+                  <li key={index}><Link underline="none" color="inherit" href={product.url} target="_blank">{product.name}</Link></li>
+                ))}
+              </Box>
             </Box>
           </Grid>
-          <Grid xs={4} sm={4}>
-            <Box component="ul" aria-labelledby="category-c" fontSize={14}>
-              <li>Link 3.1</li>
-              <li>Link 3.2</li>
-              <li>Link 3.3</li>
+          <Grid xs={4}>
+            <Box sx={{display: 'flex', justifyContent: 'center', "@media screen and (max-width:490px)": {fontSize: 12}}}>
+              <Box component="ul" aria-labelledby="category-c" sx={{ pl: 2 }}>
+                <li><Link underline="none" color="inherit" href='https://highschl.educa.nagoya-u.ac.jp/' target="_blank">高校HP</Link></li>
+                <li><Link underline="none" color="inherit" href='https://www.denpa.ac.jp/' target="_blank">専門学校HP</Link></li>
+              </Box>
             </Box>
           </Grid>
         </Grid>
-        <Grid container sx={{display: {"xs": "none", "md": "flex"}}} justifyContent="right">
+        <Grid
+          xs={12}
+          container
+          justifyContent="right"
+          alignItems="center"
+          flexDirection={{ xs: 'column', md: 'row' }}
+          sx={{ fontSize: '12px', marginRight: {"md": 2}}}
+        >
+          <Grid container columnSpacing={1} sx={{ order: { xs: 1, sm: 2 } }}>
           <Grid>
             <IconButton href='https://twitter.com/isotis_1216' target='_blank'>
               <TwitterIcon />
@@ -58,9 +72,10 @@ export default function Footer({setValue}) {
               <GitHubIcon />
             </IconButton>
           </Grid>
+          </Grid>
         </Grid>
       </Grid>
-    </Container>
+    </Box>
     </AppBar>
   );
 };
