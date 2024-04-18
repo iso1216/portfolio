f60e7327912919bc695b8455f208b235cee80042
MIST1216 <taishi2002i@gmail.com>
2024-04-18 10:34:43 +0900

    見た目の調整

--- src/components/Products.js
+++ src/components/Products.js
@@ -9,18 +9,20 @@ export default function Products(){
       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
         <Box width={'70%'}>
         {productsData.map((product, index) => (
-          <Box key={index} marginBottom={3}>
+          <Box key={index} marginBottom={3} sx={{borderColor: "gray", border: 2, borderRadius: 2, p: 3}}>
           { matches ? 
-            <><Typography variant="h4" paddingBottom={1}>{product.name}</Typography><Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                <Box sx={{ width: '80%' }}>
+            <><Typography variant="h4" paddingBottom={1}>{product.name}</Typography>
+              <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Box sx={{ width: '90%' }}>
                   <Typography variant="h6" paddingBottom={1}>{product.explanation}</Typography>
-                  <Typography variant="body1" sx={{ whiteSpace: 'pre-line' }}>{product.detail}</Typography>
+                  <Typography variant="body2" sx={{ whiteSpace: 'pre-line' }} paddingBottom={1}>{product.detail}</Typography>
                   github：<Link href={product.url} target="_blank" underline="none" sx={{ "@media screen and (max-width:400px)": { fontSize: 12 }, wordBreak: 'break-all' }}>{product.url}</Link><br />
                   {product.url2 ? <>アプリ：<Link href={product.url2} target="_blank" underline="none" sx={{ "@media screen and (max-width:400px)": { fontSize: 12 }, wordBreak: 'break-all' }}>{product.url2}</Link><br /></> : ''}
                   {product.img ? <img src={product.img} alt={product.name} className="productImage" /> : ''}
                 </Box>
               </Box></>
-            : <><Typography variant="h6" paddingBottom={1}>{product.name}</Typography><Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            : <><Typography variant="h6" paddingBottom={1}>{product.name}</Typography>
+              <Box sx={{ display: 'flex', justifyContent: 'center' }}>
                 <Box sx={{ width: '80%' }}>
                   <Typography variant="body1" paddingBottom={1}>{product.explanation}</Typography>
                   <Typography variant="body2" sx={{ whiteSpace: 'pre-line' }}>{product.detail}</Typography>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -31,6 +31,7 @@ export default function Profile(){
         setRotateImage(!rotateImage);
       }
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [keyHistoryList]);
   
 
